6195373f2491761cce9eceff092531421bf87f59
Javier <fcogzz.r@gmail.com>
2019-09-01 15:12:10 -0500

    [chore]: import react.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
