73a92055a720b19aea671a57da897211559ebd6a
Javier <fcogzz.r@gmail.com>
2019-09-01 15:19:51 -0500

    [component]: header added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './bootstrap.min.css';
-
+import Header from './components/Header'
 class App extends Component {
   state = {
 
@@ -9,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        
+        <Header
+          title='Pet Manager'
+        />
       </div>
     )
   }
