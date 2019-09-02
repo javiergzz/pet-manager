3e30eef72673c2e30149741295c72a8aa25e5a22
Javier <fcogzz.r@gmail.com>
2019-09-02 08:16:38 -0500

    [hot-fix]: eval if there any appointment

--- src/components/AppointmentList.js
+++ src/components/AppointmentList.js
@@ -2,24 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Appointment from './Appointment';
 
-const AppointmentList = ({appointments, deleteAppointment}) => (
-  <div className="card mt-2 py-5">
-    <div className="card-body">
-      <h2 className="card-title text-center">
-        Manage your appointments
-      </h2>
-      <div className="appointment-list">
-        {appointments.map((appointment) => (
-          <Appointment
-            key={appointment.id}
-            appointment={appointment}
-            deleteAppointment={deleteAppointment}
-          />
-        ))}
+const AppointmentList = ({appointments, deleteAppointment}) => {
+  // show message if is there any appointments
+  const message = Object.keys(appointments).length === 0 ? 'No appointments yet' : 'Manage your appointments';
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">
+          {message}
+        </h2>
+        <div className="appointment-list">
+          {appointments.map((appointment) => (
+            <Appointment
+              key={appointment.id}
+              appointment={appointment}
+              deleteAppointment={deleteAppointment}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 AppointmentList.propTypes = {
   appointments: PropTypes.array.isRequired,
